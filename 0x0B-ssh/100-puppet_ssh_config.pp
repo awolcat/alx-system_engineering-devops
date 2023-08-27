0ded3538ed69af89be6215fa4b5e3c5ad96d9981
Albert <albertirura11@gmail.com>
2023-08-27 20:21:35 +0300

    puppet ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Client ssh config file
 augeas { 'ssh_config':
   incl    => '/etc/ssh/ssh_config',
-  lens    => 'Ssh',
+  lens    => 'Ssh.lns',
   changes => [
-      "touch /etc/ssh/ssh_config/PasswordAuthentication",
-      "set /etc/ssh/ssh_config/PasswordAuthentication no",
-      "touch /etc/ssh/ssh_config/IdentityFile",
-      "set /etc/ssh/ssh_config/IdentityFile ~/.ssh/school",
+      "touch Host/PasswordAuthentication",
+      "set Host/PasswordAuthentication no",
+      "touch Host/IdentityFile",
+      "set Host/IdentityFile ~/.ssh/school",
     ],
 }
