86a2693775c83814aec299017e0823eb393b324c
Albert <albertirura11@gmail.com>
2023-09-05 18:00:13 +0300

    nginx puppet config

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,15 @@
+# Configure new server with nginx
+# Install nginx
 exec { 'sudo apt-get install -y nginx':
   creates => '/var/www/html',
 }
 
+# Modify site config
 file_line { 'custom header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   line   => "\tadd_header X-Served-By ${hostname};",
   after  => 'server_name _;',
 }
-
+# Restart nginx
 exec { 'sudo service nginx restart' : }
