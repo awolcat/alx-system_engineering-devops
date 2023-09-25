a3604ed8aece22bd67c04e6db18a38603dcceaca
Albert <albertirura11@gmail.com>
2023-09-25 17:02:38 +0300

    RESTful api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""REST API practice"""
+import requests
+import sys
+
+
+id = int(sys.argv[1])
+url = 'https://jsonplaceholder.typicode.com/todos/'
+todos = requests.get(url)
+totalTasks = 0
+completed = 0
+taskTitles = ""
+for todo in todos.json():
+    if todo.get('userId') == id:
+        totalTasks += 1
+        title = todo.get('title')
+        title = f'\t {title}\n'
+        taskTitles += title
+        if todo.get('completed'):
+            completed += 1
+name = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+name = name.json().get('name')
+output = f'Employee {name} is done with tasks({completed}/{totalTasks}):'
+print(output)
+print(taskTitles, end="")
