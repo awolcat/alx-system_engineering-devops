a85f75c84218ded9605705e4296c78e37e1b4048
Albert <albertirura11@gmail.com>
2023-10-11 22:19:15 +0300

    debug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -6,5 +6,5 @@ $file = '/var/www/html/wp-settings.php'
 
 exec { 'replace_string':
   command => "sed -i 's/phpp/php/g' ${file}",
-  path	  => ['/bin', '/usr/bin']
+  path    => ['/bin','/usr/bin']
 }
