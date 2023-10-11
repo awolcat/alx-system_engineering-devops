5b90d4b3043d9ef9d9d837e36c5872190a2a96e3
Albert <albertirura11@gmail.com>
2023-10-11 09:42:50 +0300

    python requests

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module defines a single function"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """This function will send a request
+        to the reddit api.
+        params:
+            subreddit: string identifying a subreddit
+        return:
+            number of subs
+    """
+    uri = f'https://www.reddit.com/r/{subreddit}/about.json'
+    custom = {'User-Agent': 'My Agent'}
+    response = requests.get(uri, headers=custom, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json().get('data')
+        subs = data.get('subscribers')
+        return subs
+    return 0
