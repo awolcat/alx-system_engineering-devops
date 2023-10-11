53b7081a3251ddde4f942eb52839d56235adb489
Albert <albertirura11@gmail.com>
2023-10-11 21:31:09 +0300

    debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# Puppet script
+
+$file = '/var/www/html/wp-settings.php'
+
+# Replace string
+
+exec { 'replace_string':
+  command => "sed -i 's/phpp/php/g' ${file}",
+  path	  => ['/bin', '/usr/bin']
+}
